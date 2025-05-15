7eca7157dcb94a4503d257d8a40dad412c8c849a
qwes3d <uratation@gmail.com>
2025-05-15 05:11:50 +0000

    fix: update script source from members.js to members.json and adjust image dimensions in directory

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -50,7 +50,7 @@ function displayMembers(members) {
     card.classList.add("member-card");
 
     card.innerHTML = `
-      <img src="images/${member.image}" alt="${member.name} logo">
+      <img src="images/${member.image}" alt="${member.name} logo" width="150" height="150">
       <h3>${member.name}</h3>
       <p>${member.description}</p>
       <p><strong>Address:</strong> ${member.address}</p>
